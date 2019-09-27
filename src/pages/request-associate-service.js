a8d82fffc2eb59f65c151c62067f2970a834a261
PhosMobile Dell1 <hello@phosmobile.com>
2019-09-27 17:44:20 +0100

    bug fixes

--- src/pages/request-associate-service.js
+++ src/pages/request-associate-service.js
@@ -9,13 +9,9 @@ export default class ExpertSpecificApplication extends Component {
     constructor(props){
         super(props)
         this.state = {
-            id:this.props.location.state.id
         }
     }
 
-    componentDidMount(){
-        console.log(this.props.location.state.id);
-    }
   render() {
     return (
       <Layout>
